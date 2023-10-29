cf30762bd65c9218e26c18e2dbc4e0b5ff557033
Zikwan Ismail <zikwanismail5@gmail.com>
2023-10-29 21:25:19 +0700

    BikinLogin

---
+++ app/Controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Php;
+
+class About extends BaseController
+{
+    public function index()
+    {
+        echo"Ini Controller Coba";
+
+    }
+}
